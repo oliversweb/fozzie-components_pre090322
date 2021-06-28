31991cac6a69cbdd44e157d2db0b2102673cca9d
billy.oliver <billy.oliver@just-eat.com>
2021-06-28 17:14:56 +0100

    f-analytics - Re-indented tests

--- packages/services/f-analytics/src/mixins/_tests/analytics.test.js
+++ packages/services/f-analytics/src/mixins/_tests/analytics.test.js
@@ -54,144 +54,140 @@ let wrapper;
 let component;
 
 describe('Analytics', () => {
-    let preparePageMock;
-    let prepareAnalyticsMock;
-    let pushAnalyticsMock;
-
-    beforeEach(() => {
-        // Arrange
-        component = {
-            render () {},
-            mixins: [analyticsMixin]
-        };
-
-        preparePageMock = jest.spyOn(component.mixins[0].methods, 'preparePage').mockImplementation();
-        prepareAnalyticsMock = jest.spyOn(component.mixins[0].methods, 'prepareAnalytics').mockImplementation();
-        pushAnalyticsMock = jest.spyOn(component.mixins[0].methods, 'pushAnalytics').mockImplementation();
-
-        wrapper = shallowMount(
-            component,
-            {
-                propsData,
-                store: createStore(),
-                localVue,
-                mocks: {
-                    $cookies
-                }
-            }
-        );
-    });
-
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
-    xit('should be defined', () => {
-        expect(wrapper.exists()).toBe(true);
-    });
-
     describe('mounted ::', () => {
-        xit('should make a call to `preparePage`', () => {
+        let preparePageMock;
+        let prepareAnalyticsMock;
+        let pushAnalyticsMock;
+
+        beforeEach(() => {
+            // Arrange & Act
+            component = {
+                render () {},
+                mixins: [analyticsMixin]
+            };
+
+            preparePageMock = jest.spyOn(component.mixins[0].methods, 'preparePage').mockImplementation();
+            prepareAnalyticsMock = jest.spyOn(component.mixins[0].methods, 'prepareAnalytics').mockImplementation();
+            pushAnalyticsMock = jest.spyOn(component.mixins[0].methods, 'pushAnalytics').mockImplementation();
+
+            wrapper = shallowMount(
+                component,
+                {
+                    propsData,
+                    store: createStore(),
+                    localVue,
+                    mocks: {
+                        $cookies
+                    }
+                }
+            );
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('should make a call to `preparePage`', () => {
             // Assert
             expect(preparePageMock).toHaveBeenCalled();
         });
 
-        xit('should make a call to `prepareAnalytics`', () => {
+        it('should make a call to `prepareAnalytics`', () => {
             // Assert
             expect(prepareAnalyticsMock).toHaveBeenCalled();
         });
 
-        xit('should make a call to `pushAnalytics`', () => {
+        it('should make a call to `pushAnalytics`', () => {
             // Assert
             expect(pushAnalyticsMock).toHaveBeenCalled();
         });
     });
-});
 
-describe('created ::', () => {
-    beforeEach(() => {
-        // Arrange
-        component = {
-            render () {},
-            mixins: [analyticsMixin]
-        };
+    describe('created ::', () => {
+        beforeEach(() => {
+            // Arrange
+            component = {
+                render () {},
+                mixins: [analyticsMixin]
+            };
 
-        jest.spyOn(component.mixins[0].methods, 'preparePage').mockImplementation();
-        jest.spyOn(component.mixins[0].methods, 'prepareAnalytics').mockImplementation();
-        jest.spyOn(component.mixins[0].methods, 'pushAnalytics').mockImplementation();
-    });
+            jest.spyOn(component.mixins[0].methods, 'preparePage').mockImplementation();
+            jest.spyOn(component.mixins[0].methods, 'prepareAnalytics').mockImplementation();
+            jest.spyOn(component.mixins[0].methods, 'pushAnalytics').mockImplementation();
+        });
 
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
 
-    it('should not set the \'serverside only\' plaformData properties if clientside', () => {
-        // Arrange & Act
-        jest.spyOn(component.mixins[0].methods, 'isServerSide').mockReturnValue(false);
-        wrapper = shallowMount(
-            component,
-            {
-                propsData,
-                store: createStore(),
-                localVue,
-                mocks: {
-                    $cookies
+        it('should not set the \'serverside only\' plaformData properties if clientside', () => {
+            // Arrange & Act
+            jest.spyOn(component.mixins[0].methods, 'isServerSide').mockReturnValue(false);
+            wrapper = shallowMount(
+                component,
+                {
+                    propsData,
+                    store: createStore(),
+                    localVue,
+                    mocks: {
+                        $cookies
+                    }
                 }
-            }
-        );
+            );
 
-        // Assert
-        expect(wrapper.vm.platformData.environment).toBe('');
-        expect(wrapper.vm.platformData.version).toBe('');
-        expect(wrapper.vm.platformData.instancePosition).toBe('');
-        expect(wrapper.vm.platformData.jeUserPercentage).toBe(0);
-    });
+            // Assert
+            expect(wrapper.vm.platformData.environment).toBe('');
+            expect(wrapper.vm.platformData.version).toBe('');
+            expect(wrapper.vm.platformData.instancePosition).toBe('');
+            expect(wrapper.vm.platformData.jeUserPercentage).toBe(0);
+        });
 
-    it('should set the \'serverside only\' plaformData properties with appropriate defaults if serverside but values not available', () => {
-        // Arrange & Act
-        jest.spyOn(component.mixins[0].methods, 'isServerSide').mockReturnValue(true);
-        wrapper = shallowMount(
-            component,
-            {
-                propsData,
-                store: createStore(),
-                localVue,
-                mocks: {
-                    $cookies
+        it('should set the \'serverside only\' plaformData properties with appropriate defaults if serverside but values not available', () => {
+            // Arrange & Act
+            jest.spyOn(component.mixins[0].methods, 'isServerSide').mockReturnValue(true);
+            wrapper = shallowMount(
+                component,
+                {
+                    propsData,
+                    store: createStore(),
+                    localVue,
+                    mocks: {
+                        $cookies
+                    }
                 }
-            }
-        );
+            );
 
-        // Assert
-        expect(wrapper.vm.platformData.environment).toBe('localhost');
-        expect(wrapper.vm.platformData.version).toBe('0.0.0.0');
-        expect(wrapper.vm.platformData.instancePosition).toBe('N/A');
-        expect(wrapper.vm.platformData.jeUserPercentage).toBe(0);
-    });
+            // Assert
+            expect(wrapper.vm.platformData.environment).toBe('localhost');
+            expect(wrapper.vm.platformData.version).toBe('0.0.0.0');
+            expect(wrapper.vm.platformData.instancePosition).toBe('N/A');
+            expect(wrapper.vm.platformData.jeUserPercentage).toBe(0);
+        });
 
-    it('should set the \'serverside only\' plaformData properties with appropriate values if serverside and available', () => {
-        // Arrange & Act
-        process.env.justEatEnvironment = 'testing123';
-        process.env.FEATURE_VERSION = '4.3.2.1';
-        process.env.INSTANCE_POSITION = '099';
-        $cookies.get = jest.fn(() => 99);
-        jest.spyOn(component.mixins[0].methods, 'isServerSide').mockReturnValue(true);
-        wrapper = shallowMount(
-            component,
-            {
-                propsData,
-                store: createStore(),
-                localVue,
-                mocks: {
-                    $cookies
+        it('should set the \'serverside only\' plaformData properties with appropriate values if serverside and available', () => {
+            // Arrange & Act
+            process.env.justEatEnvironment = 'testing123';
+            process.env.FEATURE_VERSION = '4.3.2.1';
+            process.env.INSTANCE_POSITION = '099';
+            $cookies.get = jest.fn(() => 99);
+            jest.spyOn(component.mixins[0].methods, 'isServerSide').mockReturnValue(true);
+            wrapper = shallowMount(
+                component,
+                {
+                    propsData,
+                    store: createStore(),
+                    localVue,
+                    mocks: {
+                        $cookies
+                    }
                 }
-            }
-        );
+            );
 
-        // Assert
-        expect(wrapper.vm.platformData.environment).toBe('testing123');
-        expect(wrapper.vm.platformData.version).toBe('4.3.2.1');
-        expect(wrapper.vm.platformData.instancePosition).toBe('099');
-        expect(wrapper.vm.platformData.jeUserPercentage).toBe(99);
+            // Assert
+            expect(wrapper.vm.platformData.environment).toBe('testing123');
+            expect(wrapper.vm.platformData.version).toBe('4.3.2.1');
+            expect(wrapper.vm.platformData.instancePosition).toBe('099');
+            expect(wrapper.vm.platformData.jeUserPercentage).toBe(99);
+        });
     });
 });
